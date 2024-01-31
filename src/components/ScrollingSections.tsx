9b7f6ca303ad0f470f73510547b0013a1b93683e
Rahul Rahman <rahul@rahuls-mbp.Dlink>
2024-01-31 22:54:12 +0600

    refactor scrolling sections

--- src/components/ScrollingSections.tsx
+++ src/components/ScrollingSections.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Scrollama, Step } from 'react-scrollama';
 import ScrollingCard from './ScrollingCard';
 
@@ -8,91 +8,87 @@ interface ScrollingSectionsProps {
   backgroundImagePaths: string[];
 }
 
-interface ScrollingSectionsState {
-  activeStepIndex: number;
-  backgroundStyles: React.CSSProperties[];
+interface BackgroundStyle {
+  backgroundImage: string;
+  opacity: number;
+  transition: string;
+  position: 'fixed';
+  width: string;
+  height: string;
+  top: number;
+  left: number;
+  backgroundSize: string;
+  backgroundPosition: string;
+  zIndex: number;
 }
 
-class ScrollingSections extends PureComponent<ScrollingSectionsProps, ScrollingSectionsState> {
-  constructor(props: ScrollingSectionsProps) {
-    super(props);
+const ScrollingSections = ({ id, steps, backgroundImagePaths }: ScrollingSectionsProps) => {
+  const [activeStepIndex, setActiveStepIndex] = useState(0);
+  const [backgroundStyles, setBackgroundStyles] = useState<BackgroundStyle[]>([]);
 
+  useEffect(() => {
     // Initialize background styles with opacity 0
-    const backgroundStyles = props.backgroundImagePaths.map((path) => ({
-      backgroundImage: `url(${path})`,
-      opacity: 0,
-      transition: 'opacity 0.375s ease-in-out',
-      position: 'fixed' as 'fixed',
-      width: '100%',
-      height: '100vh',
-      top: 0,
-      left: 0,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
-      zIndex: -1,
+    const initialBackgroundStyles = backgroundImagePaths.map(
+      (path) =>
+        ({
+          backgroundImage: `url(${path})`,
+          opacity: 0,
+          transition: 'opacity 0.375s ease-in-out',
+          position: 'fixed',
+          width: '100%',
+          height: '100vh',
+          top: 0,
+          left: 0,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center',
+          zIndex: -1,
+        }) as BackgroundStyle,
+    );
+
+    setBackgroundStyles(initialBackgroundStyles);
+  }, [backgroundImagePaths]);
+
+  const onStepEnter = ({ data }: { data: number }) => {
+    const updatedStyles = backgroundStyles.map((style, index) => ({
+      ...style,
+      opacity: index === data ? 1 : 0,
     }));
-
-    this.state = {
-      activeStepIndex: 0,
-      backgroundStyles,
-    };
-  }
-
-  onStepEnter = ({ data }: { data: number }) => {
-    this.setState((prevState) => {
-      const updatedStyles = prevState.backgroundStyles.map((style, index) => ({
-        ...style,
-        opacity: index === data ? 1 : 0,
-      }));
-      return {
-        activeStepIndex: data,
-        backgroundStyles: updatedStyles,
-      };
-    });
+    setActiveStepIndex(data);
+    setBackgroundStyles(updatedStyles);
   };
 
-  onStepExit = ({ data, direction }: { data: number; direction: 'up' | 'down' }) => {
+  const onStepExit = ({ data, direction }: { data: number; direction: 'up' | 'down' }) => {
     // If we're scrolling up and exiting the current active step, fade out the current step's background
-    if (direction === 'up' && data === this.state.activeStepIndex) {
-      this.setState((prevState) => {
-        const updatedStyles = prevState.backgroundStyles.map((style, index) => ({
-          ...style,
-          opacity: index === data - 1 ? 1 : 0,
-        }));
-        return {
-          activeStepIndex: data - 1,
-          backgroundStyles: updatedStyles,
-        };
-      });
+    if (direction === 'up' && data === activeStepIndex) {
+      const updatedStyles = backgroundStyles.map((style, index) => ({
+        ...style,
+        opacity: index === data - 1 ? 1 : 0,
+      }));
+      setActiveStepIndex(data - 1);
+      setBackgroundStyles(updatedStyles);
     }
   };
 
-  renderBackgrounds() {
-    return this.state.backgroundStyles.map((style, index) => <div key={index} style={style} />);
-  }
+  const renderBackgrounds = () => {
+    return backgroundStyles.map((style, index) => <div key={index} style={style} />);
+  };
 
-  render() {
-    const { steps, id } = this.props;
-
-    return (
-      <div id={id} style={{ position: 'relative', overflow: 'hidden' }}>
-        {this.renderBackgrounds()}
-        <Scrollama onStepEnter={this.onStepEnter} onStepExit={this.onStepExit}>
-          {steps.map((stepContent, index) => (
-            <Step data={index} key={index}>
-              <div
-                style={{ minHeight: '150vh', display: 'flex', alignItems: 'center', zIndex: -1 }}
-              >
-                <div className='text-white p-[1rem] md:p-[2rem] md:max-w-[40%] lg:max-w-[35%] max-w-[95%] md:m-[5rem] m-[1rem]'>
-                  <ScrollingCard>{stepContent}</ScrollingCard>
-                </div>
+  return (
+    <div id={id} style={{ position: 'relative', overflow: 'hidden' }}>
+      {renderBackgrounds()}
+      <Scrollama onStepEnter={onStepEnter} onStepExit={onStepExit}>
+        {steps.map((stepContent, index) => (
+          <Step data={index} key={index}>
+            <div style={{ minHeight: '150vh', display: 'flex', alignItems: 'center', zIndex: -1 }}>
+              <div className='text-white p-[1rem] md:p-[2rem] md:max-w-[40%] lg:max-w-[35%] max-w-[95%] md:m-[5rem] m-[1rem]'>
+                <ScrollingCard>{stepContent}</ScrollingCard>
               </div>
-            </Step>
-          ))}
-        </Scrollama>
-      </div>
-    );
-  }
-}
+            </div>
+          </Step>
+        ))}
+      </Scrollama>
+    </div>
+  );
+};
 
 export default ScrollingSections;
--- src/utils/transformGeoJSON.ts
+++ src/utils/transformGeoJSON.ts
@@ -132,116 +132,116 @@ const dataPointGenerator = (
     ChartId.TXAccess === chartId
   ) {
     return (
-      parseInt(aggregatedChoroplethData[geoId]['no_internet']['households']) /
-      parseInt(aggregatedChoroplethData[geoId]['total_households']['households'])
+      parseInt(aggregatedChoroplethData?.[geoId]?.['no_internet']?.['households']) /
+      parseInt(aggregatedChoroplethData?.[geoId]?.['total_households']?.['households'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.hispeedShare &&
     ChartId.TXAdoption === chartId
   ) {
-    return (parseFloat(aggregatedChoroplethData[geoId]['ALL']['hispeed_share'])).toFixed(2);
+    return (parseFloat(aggregatedChoroplethData?.[geoId]?.['ALL']?.['hispeed_share'])).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.lowIncomeHispeedShare &&
     ChartId.TXAdoption === chartId
   ) {
-    return (parseFloat(aggregatedChoroplethData[geoId]['LOW_INCOME']['hispeed_share'])).toFixed(2);
+    return (parseFloat(aggregatedChoroplethData?.[geoId]?.['LOW_INCOME']?.['hispeed_share'])).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.lowIncomeInternetwithdeviceshare &&
     ChartId.TXAdoption === chartId
   ) {
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['LOW_INCOME']['internet_with_device_share'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['LOW_INCOME']?.['internet_with_device_share'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.internetwithdeviceshare &&
     ChartId.TXAdoption === chartId
   ) {
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['ALL']['internet_with_device_share'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['ALL']?.['internet_with_device_share'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.lowIncomeInternetSmartphoneOnlyShare &&
     ChartId.TXAdoption === chartId
   ) {
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['LOW_INCOME']['internet_smartphone_only_share'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['LOW_INCOME']?.['internet_smartphone_only_share'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.internetSmartphoneOnlyShare &&
     ChartId.TXAdoption === chartId
   ) {
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['ALL']['internet_smartphone_only_share'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['ALL']?.['internet_smartphone_only_share'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.costAsBarrier &&
     ChartId.TXCostBarrier === chartId
   ) {
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['hid13_1']['percent'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['hid13_1']?.['percent'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.broadbandShare &&
     ChartId.TXSubscription === chartId
   ) {
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['ALL']['hispeed_share'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['ALL']?.['hispeed_share'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.digitalLiteracySkills &&
     ChartId.TXDigitalLiteracy === chartId
   ) {
     return (100 - 
-      parseFloat(aggregatedChoroplethData[geoId]['Not comfortable at all']['percent'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['Not comfortable at all']?.['percent'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.acpEligibleEnrolled &&
     ChartId.TXACP === chartId
   ) {
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['percent'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['percent'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.cybersecurityAwareness &&
     ChartId.TXCybersecurityAwareness === chartId
   ) {
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['hid11']['percent'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['hid11']?.['percent'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.publicResourceAccess &&
     ChartId.TXPublicResourceAccess === chartId
   ) {
     return (100 - 
-      parseFloat(aggregatedChoroplethData[geoId]['hid7_1']['percent'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['hid7_1']?.['percent'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.bslUnserved &&
     ChartId.TXBSL === chartId
   ) {    
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['bsl_unserved'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['bsl_unserved'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.bslUnderserved &&
     ChartId.TXBSL === chartId
   ) {    
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['bsl_underserved'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['bsl_underserved'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.costOver100 &&
     ChartId.TXCost100 === chartId
   ) {    
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['$100-$10000']['percent_of_total'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['$100-$10000']?.['percent_of_total'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.cybersecurityConfidence &&
     ChartId.TXCybersecurityConfidence === chartId
   ) {    
     return (
-      parseFloat(aggregatedChoroplethData[geoId]['Yes']['percent'])
+      parseFloat(aggregatedChoroplethData?.[geoId]?.['Yes']?.['percent'])
     ).toFixed(2);
   } else if (
     dataPointGeneratorName === DataPointGeneratorName.noHispeedShareTract &&
