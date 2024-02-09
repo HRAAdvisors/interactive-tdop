96054698c5c92164d6c3632efc62a2d6897f52ca
“eddiejoe-antonio” <108960000+eddiejoe-antonio@users.noreply.github.com>
2024-02-08 19:54:07 -0500

    Updated quoteblock and sizing for hero

--- src/components/QuoteBlock.tsx
+++ src/components/QuoteBlock.tsx
@@ -1,14 +1,20 @@
 const QuoteBlock = ({ quote }: { quote: string }) => {
   return (
-    <div className='relative mx-auto shadow-sm border-b-4 border-t-4 bg-[#002768] border-b-[#BE0B31] border-t-[#ececec] text-white'>
+    <div className='relative mx-auto max-w-4xl shadow-lg'>
       {/* Outer card (colored background) */}
-      <div className='absolute inset-0 transform translate-y-6 translate-x-6 bg-[] z-0'></div>
+      <div className='absolute inset-0 transform translate-y-1 translate-x-1 bg-[#BE0B31] z-0'></div>
+
       {/* Inner card (white foreground) */}
-      {/* <span className='absolute top-0 left-0 text-4xl lg:text-6xl translate-y-4 translate-x-4'>
+      <div className='relative bg-[#FFFDF6] p-8 z-10 border-4 border-[#002768]'>
+        {/* Quotation marks */}
+        <span className='absolute top-0 left-0 text-6xl text-[#BE0B31] transform translate-y-2 translate-x-2'>
           “
-        </span> */}
-      <p className='text-lg lg:text-lg my-8 relative p-10'>"{quote}"</p>
-      {/* <span className='absolute bottom-1 right-4 text-4xl lg:text-6xl'>”</span> */}
+        </span>
+        <p className='text-lg lg:text-xl my-4 mx-4 relative'>{quote}</p>
+        <span className='absolute bottom-0 right-0 text-6xl text-[#BE0B31] -translate-x-4 translate-y-4'>
+          ”
+        </span>
+      </div>
     </div>
   );
 };
