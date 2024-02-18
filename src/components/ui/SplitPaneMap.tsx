01aedf92619de62606ac6ebf20ed37e1070c4227
Rahul Rahman <rahul@rahuls-mbp.Dlink>
2024-02-18 23:04:39 +0600

    Fixed Zoom For Split Pane

--- src/components/ui/SplitPaneMap.tsx
+++ src/components/ui/SplitPaneMap.tsx
@@ -1,5 +1,6 @@
-import { MouseEventHandler, useRef, useState } from 'react';
+import { EventHandler, MouseEventHandler, SyntheticEvent, useRef, useState } from 'react';
 import ChoroplethMap, { ChoroplethMapProps } from './ChoroplethMap';
+import mapboxgl from 'mapbox-gl';
 
 interface SplitPaneMapProps {
   leftMapProps: Partial<ChoroplethMapProps>;
@@ -14,19 +15,31 @@ const SplitPaneMap = ({ leftMapProps, righMapProps, containerClassName }: SplitP
   const [center, setCenter] = useState<[number, number]>([-98.491142, 29.424349]);
   const [zoom, setZoom] = useState<number>(10);
 
-  const handleDragStart: MouseEventHandler = (e) => {
+  const handleDragStart: EventHandler<SyntheticEvent<any, TouchEvent | MouseEvent>> = (e) => {
     e.preventDefault();
     document.addEventListener('mousemove', handleDragMove);
     document.addEventListener('mouseup', handleDragEnd);
+    document.addEventListener('touchmove', handleTouchMove);
+    document.addEventListener('touchend', handleDragEnd);
   };
 
   const handleDragMove: (this: Document, ev: MouseEvent) => any = (e) => {
+    handleMove(e.clientX);
+  };
+
+  const handleTouchMove = (e: TouchEvent) => {
+    if (e.touches.length === 1) {
+      e.preventDefault();
+      handleMove(e.touches[0].clientX);
+    }
+  };
+
+  const handleMove = (clientX: number) => {
     if (dividerRef.current && containerRef.current) {
       const dividerRect = dividerRef.current.getBoundingClientRect();
       const containerRect = containerRef.current.getBoundingClientRect();
-      const clientx = e.clientX - containerRect.left;
-
-      const leftPaneWidth = ((clientx - dividerRect.width / 2) / containerRect.width) * 100;
+      const x = clientX - containerRect.left;
+      const leftPaneWidth = ((x - dividerRect.width / 2) / containerRect.width) * 100;
       if (leftPaneWidth <= 100 && leftPaneWidth >= 0) {
         setPaneWidths([leftPaneWidth, 100 - leftPaneWidth]);
       }
@@ -36,6 +49,8 @@ const SplitPaneMap = ({ leftMapProps, righMapProps, containerClassName }: SplitP
   const handleDragEnd = () => {
     document.removeEventListener('mousemove', handleDragMove);
     document.removeEventListener('mouseup', handleDragEnd);
+    document.removeEventListener('touchmove', handleTouchMove);
+    document.removeEventListener('touchend', handleDragEnd);
   };
 
   return (
@@ -60,6 +75,9 @@ const SplitPaneMap = ({ leftMapProps, righMapProps, containerClassName }: SplitP
                   setZoom(parseFloat(leftMapProps.mapRef.current.getZoom().toFixed(2)));
                 }
               }}
+              onLoad={(mapRef) => {
+                mapRef.scrollZoom.disable();
+              }}
               syncCenterAndZoom={true}
               geoJSONFeatureCollection={leftMapProps.geoJSONFeatureCollection}
               shouldTooltipShow={true}
@@ -78,6 +96,7 @@ const SplitPaneMap = ({ leftMapProps, righMapProps, containerClassName }: SplitP
           <div
             ref={dividerRef}
             onMouseDown={handleDragStart}
+            onTouchStart={handleDragStart}
             className='caret-container bg-white w-4 h-20 cursor-ew-resize'
             style={{ top: '50%', left: '-50%' }}
           ></div>
@@ -103,6 +122,10 @@ const SplitPaneMap = ({ leftMapProps, righMapProps, containerClassName }: SplitP
                   ]);
                   setZoom(parseFloat(righMapProps.mapRef.current.getZoom().toFixed(2)));
                 }
+              }}
+              onLoad={(map) => {
+                map.addControl(new mapboxgl.NavigationControl({ showCompass: false }));
+                map.scrollZoom.disable();
               }}
               toolTipClass='z-50'
               geoJSONFeatureCollection={righMapProps.geoJSONFeatureCollection}
