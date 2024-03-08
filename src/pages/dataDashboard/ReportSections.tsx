06ddf301654a23a5e0dd730a556cc617db02b3f9
“eddiejoe-antonio” <108960000+eddiejoe-antonio@users.noreply.github.com>
2024-03-08 15:41:04 -0500

    Download button!

--- src/pages/dataDashboard/ReportSections.tsx
+++ src/pages/dataDashboard/ReportSections.tsx
@@ -3,7 +3,6 @@ import { Chart as StandardChartComponent } from '@hraadvisors/standard-charts';
 import { Chart as BroadbandChartComponent } from '@hraadvisors/broadband-charts';
 import { ReportSection, ReportOutput } from '@hraadvisors/report-api-types';
 import type { StandardChart } from '@/types/StandardChart';
-
 import { useState } from 'react';
 import { Element as ScrollElement } from 'react-scroll';
 
@@ -27,6 +26,25 @@ const ReportSections = ({
 
   const { boundaries } = reportOutput;
 
+  const handleDownload = (chartData: any, fileName: string, fileType: string) => {
+    const csvData = convertToCSV(chartData);
+    const blob = new Blob([csvData], { type: fileType });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  };
+
+  const convertToCSV = (chartData: any) => {
+    const header = Object.keys(chartData[0]).join(',');
+    const rows = chartData.map((row: any) => Object.values(row).join(','));
+    return `${header}\n${rows.join('\n')}`;
+  };
+
   return (
     <ScrollElement name={`section${section.id}`}>
       <h3 className='text-black font-inter text-[1.5rem] font-normal tracking-normal leading-42 pt-12 pb-6 uppercase'>
@@ -40,11 +58,7 @@ const ReportSections = ({
         {_.map(_.chunk(filteredCharts, 2), (chartChunk, i) => (
           <div key={i} className='py-4 flex flex-wrap gap-8 w-full'>
             {_.map(chartChunk, (chart, key) => (
-              <div
-                key={key}
-                className='bg-white shadow-lg p-6 lg:z-40 w-full text-black'
-                // className='2xl:flex-1 bg-white shadow-lg p-6 md:z-40 w-full text-black'
-              >
+              <div key={key} className='bg-white shadow-lg p-6 lg:z-40 w-full text-black'>
                 {/standard-charts/.test(chart.library) && (
                   <StandardChartComponent
                     key={chart.id}
@@ -80,6 +94,14 @@ const ReportSections = ({
                     />
                   </>
                 )}
+                <div className='mt-2 hidden md:block'>
+                  <button
+                    className='bg-[#7085AD] hover:bg-[#002768] transition-colors duration-300 text-[0.9em] text-white font-bold py-2 px-3 rounded'
+                    onClick={() => handleDownload(chart.data, `chart${chart.id}.csv`, 'text/csv')}
+                  >
+                    Download the Data
+                  </button>
+                </div>
               </div>
             ))}
           </div>
